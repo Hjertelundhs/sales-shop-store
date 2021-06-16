e5f8cba2e62212b2f88b0382693c0e9b343513ff
Tobias Hjertelundh <tobiashjertelundh@Tobiass-MacBook-Pro.local>
2021-06-16 10:29:08 +0200

    Added homepage component with styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import HomePage from './homepage.component';
+import './homepage.styles.scss'
 import './App.css';
 
 function App() {
   return (
     <div>
-
+      <HomePage />
     </div>
   );
 }
