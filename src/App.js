f8b501bbc0ec6b58b4c91ef2373d68d75e512d3f
Tobias Hjertelundh <tobiashjertelundh@Tobiass-MacBook-Pro.local>
2021-06-17 09:36:34 +0200

    Added images and animations to menu-item

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import HomePage from './homepage.component';
-import './homepage.styles.scss'
+import HomePage from './pages/homepage.component';
+
+import './pages/homepage.styles.scss'
 import './App.css';
 
 function App() {
